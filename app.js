dd88ba9b30cfd7e9439214c9d792cb60a0f44017
kjhernandez <hernandezkj97@gmail.com>
2021-07-19 14:30:24 -0400

    Some changes in the button style

---
+++ app.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector('.btn');
+const input = document.querySelector('.input');
+const container = document.querySelector('.container');
+
+btn.addEventListener('click', () => {
+    container.classList.toggle('active');
+    input.focus();
+});
